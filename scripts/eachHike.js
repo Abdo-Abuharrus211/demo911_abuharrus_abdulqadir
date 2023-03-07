c431a2cf23081b0016854c765ca34a31ae04c16a
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-07 09:54:22 -0800

    Add review files

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -1,16 +1,16 @@
-function displayHikeInformation(){
+function displayHikeInformation() {
     //retreive the document id from the url
     let params = new URL(window.location.href) //get the url from the searbar
     let ID = params.searchParams.get("docID");
     console.log(ID);
 
-    db.collection("hikes").doc(ID).get().then( thisHike =>{
+    db.collection("hikes").doc(ID).get().then(thisHike => {
         hikeInfo = thisHike.data();
         hikeCode = hikeInfo.code;
         hikeName = hikeInfo.name;
 
-        document.getElementById("hikeName").innerHTML=hikeName;
-        let imgEvent = document.querySelector( ".hike-img" );
+        document.getElementById("hikeName").innerHTML = hikeName;
+        let imgEvent = document.querySelector(".hike-img");
         imgEvent.src = "../images/" + hikeCode + ".jpg";
     }
 
@@ -18,3 +18,10 @@ function displayHikeInformation(){
 
 }
 displayHikeInformation();
+
+function saveHikeDocumentIDAndRedirect() {
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
